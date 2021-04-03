9c4857ef81a771fb8904e7e19f5da730e132c072
Lenakeiz <andrea.castegnaro@outlook.com>
2021-04-04 00:51:17 +0100

    Added alternative solution to 1.1 using bitset instead vector to save time

--- Chapter1/IsUnique1_1.cpp
+++ Chapter1/IsUnique1_1.cpp
@@ -32,24 +32,39 @@ bool isUniqueChars(string str)
 bool isUniqueChars_Sort(string str)
 {
     if (str.length() > 128) return false;
-    vector<bool> ascii_set(128);
+
+    sort(str.begin(), str.end());
+        
+    for (int i = 0; (unsigned)i < str.length() - 1; i++)
+    {
+        if (str[i] == str[i + 1])
+        {
+            return false;
+        }                
+    }
+    return true;
+}
+
+//If using a bitset (http://www.cplusplus.com/reference/bitset/bitset/) you can save 8 times the space. Not sure if you increase performance because that depends on the implementation of vector<bool> and bitset
+//This is a 0(n^2) method looping to the array twice
+bool isUniqueChars_BitSet(string str)
+{
+    if (str.length() > 128) return false;
+    std::bitset<128> ascii_set;
     for (int i = 0; (unsigned)i < str.length(); i++)
     {
-        sort(str.begin(), str.end());
-        
-        for (int i = 0; (unsigned)i < str.length() - 1; i++)
+        //Treat the character as an int
+        for (int i = 0; (unsigned)i < str.length(); i++)
         {
-            if (str[i] == str[i + 1])
-            {
-                return false;
-            }                
+            int val = str[i];
+            if (ascii_set.test(val)) return false;
+            ascii_set.set(val, true);
         }
         return true;
     }
     //Assuming the string is ASCII - 128 characters
     return true;
 }
-
 int main()
 {
     vector<string> testValues = {"abcde", "mamma mia", "mMuUtT", "ciao"};
@@ -62,6 +77,11 @@ int main()
     for (auto word : testValues)
     {
         cout << "Testing: " << word << " Result: " << std::boolalpha << isUniqueChars_Sort(word) << endl;
+    }
+
+    for (auto word : testValues)
+    {
+        cout << "Testing: " << word << " Result: " << std::boolalpha << isUniqueChars_BitSet(word) << endl;
     }
 
 }
