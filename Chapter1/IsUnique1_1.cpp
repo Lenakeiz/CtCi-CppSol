3d876df66b91288f55955d1022e256fb6c567048
Lenakeiz <andrea.castegnaro@outlook.com>
2021-04-04 00:17:31 +0100

    Added solution following suggestion #132

--- Chapter1/IsUnique1_1.cpp
+++ Chapter1/IsUnique1_1.cpp
@@ -28,6 +28,28 @@ bool isUniqueChars(string str)
     return true;
 }
 
+//This is a 0(nlogn) for sorting the vector
+bool isUniqueChars_Sort(string str)
+{
+    if (str.length() > 128) return false;
+    vector<bool> ascii_set(128);
+    for (int i = 0; (unsigned)i < str.length(); i++)
+    {
+        sort(str.begin(), str.end());
+        
+        for (int i = 0; (unsigned)i < str.length() - 1; i++)
+        {
+            if (str[i] == str[i + 1])
+            {
+                return false;
+            }                
+        }
+        return true;
+    }
+    //Assuming the string is ASCII - 128 characters
+    return true;
+}
+
 int main()
 {
     vector<string> testValues = {"abcde", "mamma mia", "mMuUtT", "ciao"};
@@ -35,6 +57,11 @@ int main()
     for ( auto word : testValues)
     {
         cout << "Testing: " << word << " Result: " << std::boolalpha << isUniqueChars(word) << endl;
+    }
+
+    for (auto word : testValues)
+    {
+        cout << "Testing: " << word << " Result: " << std::boolalpha << isUniqueChars_Sort(word) << endl;
     }
 
 }
