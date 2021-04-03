b846d3738c06a2fe81d5cabf74d17f46f7c887f6
Lenakeiz <andrea.castegnaro@outlook.com>
2021-04-04 00:01:57 +0100

    Added first exercise

---
+++ Chapter1/IsUnique1_1.cpp
@@ -0,0 +1,40 @@
+// IsUnique1_1.cpp : Chapter 1 exercise 1
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <bitset>
+#include <algorithm>
+
+using namespace std;
+
+//This is a 0(n^2) method looping to the array twice
+bool isUniqueChars(string str)
+{
+    if (str.length() > 128) return false;
+    vector<bool> ascii_set(128);
+    for (int i = 0; (unsigned)i < str.length(); i++)
+    {
+        //Treat the character as an int
+        for (int i = 0; (unsigned)i < str.length(); i++)
+        {
+            int val = str[i];
+            if (ascii_set[val]) return false;
+            ascii_set[val] = true;
+        }
+        return true;
+    }
+    //Assuming the string is ASCII - 128 characters
+    return true;
+}
+
+int main()
+{
+    vector<string> testValues = {"abcde", "mamma mia", "mMuUtT", "ciao"};
+
+    for ( auto word : testValues)
+    {
+        cout << "Testing: " << word << " Result: " << std::boolalpha << isUniqueChars(word) << endl;
+    }
+
+}
