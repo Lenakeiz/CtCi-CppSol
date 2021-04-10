288b4907e22ecc9b585864acddb65047f3d33b74
Lenakeiz <andrea.castegnaro@outlook.com>
2021-04-11 00:42:48 +0100

    Adding the Urlify solution

--- Chapter1/URLify.cpp
+++ Chapter1/URLify.cpp
@@ -9,6 +9,43 @@ using namespace std;
 //Assuming ASCII
 //Ascii code of "Space" =  32
 
+//Getting the pointer to the string
+string URLifystring(string &toUrl, int sizeURL)
+{
+	string urlified = "";
+	int numSpaces = 0;
+	int i, j = 0;
+
+	for (i = 0; i < sizeURL; i++)
+	{
+		if (toUrl[i] == ' ')
+		{
+			numSpaces++;
+		}
+	}
+
+	//Going backward is a good trick so you fill up the extra spaces calculated with the urlLenght
+	int urlLenght = sizeURL + numSpaces * 2;
+	i = urlLenght - 1;
+	
+	//i writes to the position 
+	for (j = sizeURL - 1 ; j >= 0; j--)
+	{
+		if (toUrl[j] != ' ')
+		{
+			toUrl[i--] = toUrl[j];
+		}
+		else
+		{
+			toUrl[i--] = '0';
+			toUrl[i--] = '2';
+			toUrl[i--] = '%';
+		}
+	}
+	return toUrl;
+}
+
+//Wrong attempt, I didn t realize you needed to check for the number of spaces. Also did not realized that the not Urlified string has enough traling spaces to make up with the urlification
 string URLify(string toUrl, int sizeURL)
 {
 	if (sizeURL == 0 && toUrl.size() == sizeURL)
@@ -32,15 +69,17 @@ string URLify(string toUrl, int sizeURL)
 			urlified++;
 		}
 	}
+	//Wrong Attempt
+	return toUrl;
 }
 
 int main()
 {
-	vector<string> testValues = { "abcde", "mam mia", "m u UT", "ccc ia o  " };
-	vector<int>	   exactSize = { 5,9,10,12 };
+	vector<string> testValues = { "abcde", "Mr John Smith    ", "m u UT    ", "ccc ia o    " };
+	vector<int>	   exactSize = {5,13,6,8};
 	for (unsigned int i = 0; i < testValues.size(); i++)
 	{
-		cout << "URLify string of " << testValues[i] << " : " << URLify(testValues[i], exactSize[i]) << endl;
+		cout << "URLify string of " << testValues[i] << " : " << URLifystring(testValues[i], exactSize[i]) << endl;
 	}
 	return 0;
 }
